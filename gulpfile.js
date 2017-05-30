e625520327922fc28cc3e3a41a486f76321bcd65
Josue Ttito <joshjo@outlook.com>
2017-05-30 18:01:11 +0200

    added watcher less

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,16 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
+var watch = require('gulp-watch');
 var path = require('path');
 
 gulp.task('less', function () {
-  return gulp.src('./static/less/**/*.less')
+  return gulp.src('./static/less/main.less')
     .pipe(less({
       paths: [ path.join(__dirname, 'less', 'includes') ]
     }))
     .pipe(gulp.dest('./static/css'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('./static/**/*.less', ['less']);  // Watch all the .less files, then run the less task
 });
